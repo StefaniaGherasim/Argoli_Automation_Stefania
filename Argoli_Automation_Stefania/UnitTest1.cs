0fc87a5811b865447a352cf01bbf435e84c01dad
StefaniaGherasim <sstefaniadaniela@gmail.com>
2022-03-28 15:18:06 +0300

    Add project files.

---
+++ Argoli_Automation_Stefania/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Argoli_Automation_Stefania
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
