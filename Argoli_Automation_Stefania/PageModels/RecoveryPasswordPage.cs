056343ee4c226eca8274e7f36bf11ab914d6214a
StefaniaGherasim <sstefaniadaniela@gmail.com>
2022-03-30 21:18:42 +0300

    Recovery page

---
+++ Argoli_Automation_Stefania/PageModels/RecoveryPasswordPage.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Argoli_Automation_Stefania.PageModels
+{
+    public class RecoveryPasswordPage : BasePage
+    {
+
+        const string emailLabelSelector = "body > div.recovery > div > div.col-md-4 > div.form-group.mb-0 > label";//css
+        const string emailInputSelector = "emailRecovery";//id
+        const string errMessageLabelSelector = "#messageArea > h5";//css
+        const string errMsg2LabelSelector = "#messageArea > h5:nth-child(1)";//css
+        const string trimiteButtonSelector = "body > div.recovery > div > div.col-md-4 > button";//css
+        const string mesajSuccesLabel = "#messageArea > h5:nth-child(1)";//css
+
+        public RecoveryPasswordPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public string CheckEmailLabel()
+        {
+            return driver.FindElement(By.CssSelector(emailLabelSelector)).Text;
+        }
+
+        public string EmailInputLabel()
+        {
+            return driver.FindElement(By.Id(emailInputSelector)).Text;
+        }
+
+        public string ErrMsgLabel()
+        {
+            return driver.FindElement(By.CssSelector(errMessageLabelSelector)).Text;
+        }
+
+        public string ErrMsg2Label()
+        {
+            return driver.FindElement(By.CssSelector(errMsg2LabelSelector)).Text;
+        }
+
+        public string SuccesMessageLabel()
+        {
+            return driver.FindElement(By.CssSelector(mesajSuccesLabel)).Text;
+        }
+
+        public void TrimiteButton()
+        {
+            var trimiteButton = driver.FindElement(By.CssSelector(trimiteButtonSelector));
+            trimiteButton.Click();
+         }
+    }
+}
---
+++ Argoli_Automation_Stefania/Tests/RecoveryPassword/RecoveryPasswordTest.cs
@@ -0,0 +1,25 @@
+﻿using Argoli_Automation_Stefania.PageModels;
+using Argoli_Automation_Stefania.Utilities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Argoli_Automation_Stefania.Tests.RecoveryPassword
+{
+    public class RecoveryPasswordTest : BaseTest
+    {
+        string url = FrameworkConstants.GetUrl() + "/recovery";
+
+        [Test]
+        public void RecoverPasswordWithoutData()
+        {
+            testName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(testName);
+            _driver.Navigate().GoToUrl(url);
+           
+
+
+        }
+    }
+}
