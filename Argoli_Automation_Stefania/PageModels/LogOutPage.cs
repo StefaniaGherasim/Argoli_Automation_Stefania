931600536b991dea38b66a77f439afad30a3b155
StefaniaGherasim <sstefaniadaniela@gmail.com>
2022-04-01 01:28:26 +0300

    Log Out Page/test

---
+++ Argoli_Automation_Stefania/PageModels/LogOutPage.cs
@@ -0,0 +1,57 @@
+﻿using Argoli_Automation_Stefania.Utilities;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Argoli_Automation_Stefania.PageModels
+{
+    public class LogOutPage : BasePage
+    {
+
+        const string nameAccountSelector = "#navbar-navigation > ul.nav.navbar-nav.ml-xl-auto > li.nav-item.dropdown.dropdown-user > a > span";//css  
+        const string logOutSelector = "#navbar-navigation > ul.nav.navbar-nav.ml-xl-auto > li.nav-item.dropdown.dropdown-user.show > div > a:nth-child(4)";//css
+        const string myAccountSeletor = "#navbar-navigation > ul.nav.navbar-nav.ml-xl-auto > li:nth-child(3) > a";//css
+
+
+        public LogOutPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+        public string NameAccountLabel()
+        {
+
+            return driver.FindElement(By.CssSelector(nameAccountSelector)).Text;
+
+        }
+
+        public void EnterNameAccount()
+        {
+            driver.FindElement(By.CssSelector(nameAccountSelector)).Click();
+
+        }
+
+        public string LogOutLabel()
+        {
+
+            return driver.FindElement(By.CssSelector(logOutSelector)).Text;
+
+        }
+        public string MyAccountLabel()
+        {
+
+            return driver.FindElement(By.CssSelector(myAccountSeletor)).Text;
+
+        }
+
+        public void LogOutUserAccount()
+        {
+            driver.FindElement(By.CssSelector(logOutSelector)).Click();
+            var select = Utils.WaitForElementClickable(driver, 1000, (By.CssSelector(myAccountSeletor)));
+            select.Click();
+        }
+
+
+    }
+}
---
+++ Argoli_Automation_Stefania/Tests/LogOut/LogOutTest.cs
@@ -0,0 +1,45 @@
+﻿using Argoli_Automation_Stefania.PageModels;
+using Argoli_Automation_Stefania.Utilities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Argoli_Automation_Stefania.Tests.LogOut
+{
+    public class LogOutTest : BaseTest
+    {
+
+        string url = FrameworkConstants.GetUrl();
+
+        private static IEnumerable<TestCaseData> GetCredentialsDataCsv2()
+        {
+            foreach (var values in Utils.GetGenericData("TestData\\AuthenticationData.csv"))
+            {
+                yield return new TestCaseData(values);
+            }
+        }
+
+        [Test, Order(13), TestCaseSource("GetCredentialsDataCsv2")]
+        public void LogOutUserTest(string email, string password)
+        {
+            testName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(testName);
+            _driver.Navigate().GoToUrl(url);
+            MainPage mp = new MainPage(_driver);
+            mp.MoveToLoginPage();
+
+            LoginPage lp = new LoginPage(_driver);
+            lp.Login2(email, password);
+
+            LogOutPage lo = new LogOutPage(_driver);
+            Assert.AreEqual("Aron Gherasim", lo.NameAccountLabel());
+            lo.EnterNameAccount();
+            Assert.AreEqual("Deconectare", lo.LogOutLabel());
+            lo.LogOutUserAccount();
+            Thread.Sleep(1000);
+            Assert.AreEqual("Contul meu", lo.MyAccountLabel());
+        }
+    }
+}
